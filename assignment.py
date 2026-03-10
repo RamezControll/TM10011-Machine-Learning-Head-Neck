3611c93b810a409b397214562dfacbb55aa86dfa
anoukbraakhuis <A.M.Braakhuis@student.tudelft.nl>
2026-03-10 11:42:04 +0100

    Dit is preprocessing code

--- assignment.py
+++ assignment.py
@@ -6,10 +6,12 @@
 #from worcgist.load_data import load_data
 #from worclipo.load_data import load_data
 #from worcliver.load_data import load_data
-#from hn.load_data import load_data
+from hn.load_data import load_data
 #from ecg.load_data import load_data
 
 data = load_data()
 print(f'The number of samples: {len(data.index)}')
-
 print(f'The number of columns: {len(data.columns)}')
+print(data)
+
+# %%
